639a69a58cb9322304b0582301c01a089721826e
Myself-Praveen <praveen104685@gmail.com>
2026-04-20 07:45:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Derangements/count-derangements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int derangeCount(int n) {
+        vector<int> dp(n+1, 0);
+        dp[2] = 1;
+
+        for(int i = 3;i <= n; i++)
+            dp[i] = (i-1)*(dp[i-1]+dp[i-2]);
+
+        return dp[n];
+    }
+};
